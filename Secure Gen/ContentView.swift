095f3437811ce985368ca9cf1eab3d0ae6c67540
tuildes <frehsegustavo@gmail.com>
2026-03-18 20:13:39 -0300

    feat: project base

---
+++ Secure Gen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Secure Gen
+//
+//  Created by Gustavo Benitez Frehse on 18/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Secure Gen/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Secure_GenApp.swift
+//  Secure Gen
+//
+//  Created by Gustavo Benitez Frehse on 18/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
